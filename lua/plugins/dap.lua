7ab44d1aab9421897c5b9c11e405d7951bc2cab2
Jonah Collins <=>
2025-07-03 21:42:14 +0100

    Add DAP support with python and lua

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,82 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'theHamsta/nvim-dap-virtual-text',
+
+      'mfussenegger/nvim-dap-python',
+      'jbyuki/one-small-step-for-vimkind',
+    },
+    config = function()
+      local dap = require('dap')
+      local dapui = require('dapui')
+
+      require('dapui').setup()
+      require('nvim-dap-virtual-text').setup({
+        enabled = true,
+        virtual_text = true,
+        commented = true,
+        transparent = true,
+        display_variables = true,
+      })
+
+      -- Set up dapui auto loading
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      --dap.listeners.before.event_terminated.dapui_config = function()
+        --dapui.close()
+      --end
+      --dap.listeners.before.event_exited.dapui_config = function()
+        --dapui.close()
+      --end
+
+      -- Set up python support
+      dap.adapters.python = {
+        type = 'executable',
+        command = os.getenv('HOME') .. '/.venvs/debugpy/bin/python',
+        args = { '-m', 'debugpy.adapter' },
+      }
+      dap.configurations.python = {
+        {
+          type = 'python',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          pythonPath = function()
+            return '/opt/homebrew/bin/python3'
+          end,
+          console = 'integratedTerminal',
+        },
+      }
+
+
+      -- Set up lua support
+      dap.configurations.lua = { 
+        { 
+          type = 'nlua', 
+          request = 'attach',
+          name = "Attach to running Neovim instance",
+          console = 'integratedTerminal',
+        }
+      }
+      
+      dap.adapters.nlua = function(callback, config)
+        require'osv'.launch({port = 8086})
+        callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
+      end
+      -- Set up keymaps - see the github page for api bindings
+      vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {})
+      vim.keymap.set('n', '<Leader>dc', dap.continue, {})
+      vim.keymap.set('n', '<leader>dq', dapui.toggle)
+      vim.keymap.set('n', '<leader>do', require"dap".step_over, { noremap = true })
+      vim.keymap.set('n', '<leader>di', require"dap".step_into, { noremap = true })
+      
+    end,
+  },
+}
